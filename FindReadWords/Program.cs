a1a0193101847f016c2294e90629aff694c0e822
Kees Hiemstra <chi@xs4all.nl>
2020-07-10 14:19:09 +0200

    Developed FindReadWords

---
+++ FindReadWords/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindReadWords
+{
+  class Program
+  {
+    public static DateTime Time = DateTime.Now;
+    public static List<string> WordList = new List<string>();
+    public static string RandomWord = "PZLIEEGN".ToLower();
+    public static Dictionary<string, int> FoundWords = new Dictionary<string, int>();
+
+    static void Main(string[] args)
+    {
+      LoadWordList();
+
+      SearchWords();
+
+      int count = 0;
+      foreach (var word in FoundWords.OrderByDescending(x => x.Key.Length))
+      {
+        count++;
+        if (count > 5)
+        {
+          break;
+        }
+        Console.WriteLine($"{word.Key}\t{word.Key.Length}");
+      }
+      Console.WriteLine($"\nFound {FoundWords.Count} words");
+
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+
+    private static void Log(string message)
+    {
+      Console.WriteLine($"{(DateTime.Now - Time)} {message}");
+    }
+
+    private static void LoadWordList()
+    {
+      string[] wordList = Properties.Resources.WordList
+        .Replace("\r\n", "\n")
+        .Split('\n');
+      foreach (string word in wordList)
+      {
+        WordList.Add(word);
+      }
+      Log($"{WordList.Count} are loaded");
+    }
+
+    /// <summary>
+    /// Search for words with the matrix letters of 2 letters.
+    /// </summary>
+    private static void SearchWords()
+    {
+      for (int x = 0; x < RandomWord.Length; x++)
+      {
+        for (int y = 0; y < RandomWord.Length; y++)
+        {
+          if (x == y) { continue; }
+          AddFoundWord(RandomWord[x].ToString() + RandomWord[y].ToString());
+        }
+      }
+      Log($"{FoundWords.Count} words found");
+    }
+
+    /// <summary>
+    /// Does the found word exists the right letters.
+    /// </summary>
+    /// <param name="search"></param>
+    private static void AddFoundWord(string search)
+    {
+      foreach (string found in WordList.Where(x => x.Contains(search)))
+      {
+        if (!FoundWords.ContainsKey(found))
+        {
+          if (DoesLetterExist(found) && MatchWord(found))
+          {
+            FoundWords.Add(found, 1);
+          }
+        }
+        else
+        {
+          FoundWords[found]++;
+        }
+      }
+    }
+
+    private static bool DoesLetterExist(string word)
+    {
+      bool add = true;
+      foreach (char letter in word)
+      {
+        add = add && RandomWord.Contains(letter);
+        if (!add) { break; }
+      }
+      return add;
+    }
+
+    private static bool MatchWord(string word)
+    {
+      bool add = true;
+      string random = RandomWord;
+
+      foreach (char letter in word)
+      {
+        int pos = random.IndexOf(letter);
+        if (pos > 0)
+        {
+          random = random.Remove(pos, 1);
+        }
+        else
+        {
+          add = false;
+          break;
+        }
+      }
+      return add;
+    }
+
+  }
+}
---
+++ WriteWordList/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WriteWordList
+{
+  class Program
+  {
+    public static List<string> WordList = new List<string>();
+    static void Main(string[] args)
+    {
+      WordList.Add("One");
+      WordList.Add("Two");
+      WordList.Add("Three");
+
+      using (StreamWriter stream = new StreamWriter("WordList.txt"))
+      {
+        foreach (var item in WordList)
+        {
+          stream.WriteLine(item);
+        }
+      }
+
+
+      Console.Write("\nPress any key...");
+      Console.ReadKey();
+    }
+  }
+}
